3434cb76c70c80586f24a382a9529f0e9e25af77
winlogon <walker84837@gmail.com>
2025-06-18 03:49:30 +0200

    refactor: make Cooldown not a static class

--- src/main/java/org/winlogon/combatweaponryplus/Cooldown.java
+++ src/main/java/org/winlogon/combatweaponryplus/Cooldown.java
@@ -14,26 +14,25 @@ public class Cooldown {
 
     public void setCooldown(Player player, int seconds) {
         double delay = System.currentTimeMillis() + (long)seconds * 1000L;
-        this.cooldowns.put(player.getUniqueId(), delay);
+        cooldowns.put(player.getUniqueId(), delay);
     }
 
-    public static Optional<Integer> getCooldown(Player player) {
+    public Optional<Integer> getCooldown(Player player) {
         var uuid = player.getUniqueId();
-        if (!this.cooldowns.containsKey(uuid)) {
+        if (!cooldowns.containsKey(uuid)) {
             return Optional.empty();
         }
-        var currentTime = (double)System.currentTimeMillis() / 1000.0);
+        var currentTime = (double)System.currentTimeMillis() / 1000.0;
         var playerCooldown = cooldowns.get(uuid);
         var round = Math.round(playerCooldown - currentTime);
         try {
             return Optional.of(Math.toIntExact(round));
-        } catch (ArithmeticException _) {
+        } catch (ArithmeticException e) {
             return Optional.empty();
         }
     }
 
-    public static boolean checkCooldown(Player player) {
+    public boolean checkCooldown(Player player) {
         return !cooldowns.containsKey(player.getUniqueId()) || cooldowns.get(player.getUniqueId()) <= (double)System.currentTimeMillis();
     }
 }
-
