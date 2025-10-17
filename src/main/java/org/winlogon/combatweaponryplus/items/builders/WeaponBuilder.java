994ba6f26e377427f054c97199ac8e4b4e5711eb
winlogon <walker84837@gmail.com>
2025-10-17 09:29:14 +0200

    chore(attributes): strip GENERIC_ from the start

--- src/main/java/org/winlogon/combatweaponryplus/items/builders/WeaponBuilder.java
+++ src/main/java/org/winlogon/combatweaponryplus/items/builders/WeaponBuilder.java
@@ -11,22 +11,22 @@ public class WeaponBuilder extends ItemBuilder {
     }
 
     public WeaponBuilder attackDamage(double attackDamage) {
-        super.attribute(Attribute.GENERIC_ATTACK_DAMAGE, attackDamage, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlotGroup.HAND);
+        super.attribute(Attribute.ATTACK_DAMAGE, attackDamage, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlotGroup.HAND);
         return this;
     }
 
     public WeaponBuilder attackSpeed(double attackSpeed) {
-        super.attribute(Attribute.GENERIC_ATTACK_SPEED, attackSpeed, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlotGroup.HAND);
+        super.attribute(Attribute.ATTACK_SPEED, attackSpeed, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlotGroup.HAND);
         return this;
     }
 
     public WeaponBuilder movementSpeed(double movementSpeed) {
-        super.attribute(Attribute.GENERIC_MOVEMENT_SPEED, movementSpeed, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlotGroup.HAND);
+        super.attribute(Attribute.MOVEMENT_SPEED, movementSpeed, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlotGroup.HAND);
         return this;
     }
 
     public WeaponBuilder knockbackResistance(double knockbackResistance) {
-        super.attribute(Attribute.GENERIC_KNOCKBACK_RESISTANCE, knockbackResistance, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlotGroup.HAND);
+        super.attribute(Attribute.KNOCKBACK_RESISTANCE, knockbackResistance, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlotGroup.HAND);
         return this;
     }
 }
