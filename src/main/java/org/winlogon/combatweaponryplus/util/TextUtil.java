8824763052b78da229e495759a9b65967ae8a110
winlogon <walker84837@gmail.com>
2025-10-17 09:31:51 +0200

    refactor(TextUtil): use Jetbrains annotations

--- src/main/java/org/winlogon/combatweaponryplus/util/TextUtil.java
+++ src/main/java/org/winlogon/combatweaponryplus/util/TextUtil.java
@@ -4,13 +4,16 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.winlogon.retrohue.RetroHue;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class TextUtil {
-
     private static MiniMessage miniMessage;
     private static RetroHue retroHue;
 
@@ -19,19 +22,21 @@ public class TextUtil {
         retroHue = rh;
     }
 
-    public static String convertLegacyToSection(String s) {
+    public static @Nullable String convertLegacyToSection(@Nullable String s) {
         if (s == null) return null;
         // TODO: find better alternative and optimized alernative
         return ChatColor.translateAlternateColorCodes('&', s);
     }
 
-    public static String convertLegacyToSectionWithConfig(String key, FileConfiguration config) {
-        if (config == null || key == null) return null;
-        String configString = config.getString(key);
-        return convertLegacyToSection(configString);
+    public static Optional<String> convertLegacyToSectionWithConfig(@NotNull String key, @NotNull FileConfiguration config) {
+        Objects.requireNonNull(key);
+        Objects.requireNonNull(config);
+
+        return Optional.ofNullable(config.getString(key))
+            .map(TextUtil::convertLegacyToSection);
     }
 
-    public static Component convertLegacyToComponent(String s) {
+    public static @NotNull Component convertLegacyToComponent(@Nullable String s) {
         if (s == null) return Component.empty();
         if (miniMessage == null || retroHue == null) {
             return Component.text(s); // Fallback if not initialized
@@ -40,7 +45,7 @@ public class TextUtil {
         return miniMessage.deserialize(miniMessageConverted);
     }
 
-    public static List<Component> convertLegacyLoreToComponents(List<String> lore) {
+    public static @Nullable List<Component> convertLegacyLoreToComponents(@Nullable List<String> lore) {
         if (lore == null) return null;
         return lore.stream()
                 .map(TextUtil::convertLegacyToComponent)
