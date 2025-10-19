a8fedc728584ef517ba330a53673edcccc49f3bc
winlogon <walker84837@gmail.com>
2025-10-19 06:22:19 +0200

    docs: add javadoc in AttributeUtil & EnchantUtils

--- src/main/java/org/winlogon/combatweaponryplus/util/AttributeModifierUtil.java
+++ src/main/java/org/winlogon/combatweaponryplus/util/AttributeModifierUtil.java
@@ -4,12 +4,12 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.inventory.EquipmentSlotGroup;
+import org.jetbrains.annotations.NotNull;
 import org.winlogon.combatweaponryplus.CombatWeaponryPlus;
 
 import java.util.UUID;
 
 public class AttributeModifierUtil {
-
     private static NamespacedKey createNamespacedKey(String key) {
         return new NamespacedKey(CombatWeaponryPlus.getInstance(), key);
     }
@@ -18,11 +18,29 @@ public class AttributeModifierUtil {
         return new NamespacedKey(CombatWeaponryPlus.getInstance(), "");
     }
 
-    public static AttributeModifier createAttributeModifier(Attribute attribute, double amount, AttributeModifier.Operation operation, EquipmentSlotGroup slotGroup) {
-        return new AttributeModifier(createNamespacedKey(attribute.name().toLowerCase() + "_" + UUID.randomUUID().toString()), amount, operation, slotGroup);
+    /**
+     * Creates an AttributeModifier.
+     * <br><b>Implementation detail</b>: Creates a random key for the attribute
+     *
+     * @param attribute The attribute
+     * @param amount The amount
+     * @param operation The operation
+     * @param slotGroup The slot group
+     * @return The {@link AttributeModifier}
+     */
+    public static @NotNull AttributeModifier createAttributeModifier(@NotNull Attribute attribute, double amount, @NotNull AttributeModifier.Operation operation, @NotNull EquipmentSlotGroup slotGroup) {
+        var key = createNamespacedKey(attribute.name().toLowerCase() + "_" + UUID.randomUUID().toString());
+        return new AttributeModifier(key, amount, operation, slotGroup);
     }
 
-    public static AttributeModifier createAttributeModifier(Attribute attribute, double amount, AttributeModifier.Operation operation) {
+    /**
+     * Creates an AttributeModifier with {@link AttributeModifier.Operation#ADD_NUMBER}
+     *
+     * @param attribute The attribute
+     * @param amount The amount
+     * @return The {@link AttributeModifier}
+     */
+    public static @NotNull AttributeModifier createAttributeModifier(@NotNull Attribute attribute, double amount, @NotNull AttributeModifier.Operation operation) {
         return createAttributeModifier(attribute, amount, operation, EquipmentSlotGroup.ANY);
     }
 }
--- src/main/java/org/winlogon/combatweaponryplus/util/EnchantmentUtil.java
+++ src/main/java/org/winlogon/combatweaponryplus/util/EnchantmentUtil.java
@@ -5,6 +5,15 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class EnchantmentUtil {
+    /**
+     * Adds an enchantment to an item and returns it.
+     *
+     * @param item The item to add the enchantment to.
+     * @param enchantment The enchantment to add.
+     * @param level The level of the enchantment.
+     * @param ignoreLevelRestriction Whether to ignore the level restriction.
+     * @return The item with the enchantment added.
+     */
     public static ItemStack addEnchantment(ItemStack item, Enchantment enchantment, int level, boolean ignoreLevelRestriction) {
         ItemMeta meta = item.getItemMeta();
         meta.addEnchant(enchantment, level, ignoreLevelRestriction);
