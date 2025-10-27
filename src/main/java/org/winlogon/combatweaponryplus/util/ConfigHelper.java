78a975755fd2877f0b4b9dbb792355e4f75e78d0
winlogon <walker84837@gmail.com>
2025-10-27 11:31:18 +0100

    docs(ConfigHelper): add Javadoc comments

--- src/main/java/org/winlogon/combatweaponryplus/util/ConfigHelper.java
+++ src/main/java/org/winlogon/combatweaponryplus/util/ConfigHelper.java
@@ -1,6 +1,8 @@
 package org.winlogon.combatweaponryplus.util;
 
 import org.bukkit.configuration.file.FileConfiguration;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.List;
 
 public class ConfigHelper {
@@ -10,11 +12,24 @@ public class ConfigHelper {
         this.config = config;
     }
 
+    /**
+     * Checks if a path is enabled in the config.
+     * Simply calls {@link FileConfiguration#getBoolean(String, boolean)}
+     *
+     * @param path The path to check
+     * @return Whether the path is enabled
+     */
     public boolean isEnabled(String path) {
         return config.getBoolean(path, false);
     }
 
-    public boolean areEnabled(String... paths) {
+    /**
+     * Checks if all paths are enabled in the config.
+     *
+     * @param paths The paths to check
+     * @return Whether all paths are enabled
+     */
+    public boolean areEnabled(@NotNull String... paths) {
         for (var path : paths) {
             if (!config.getBoolean(path, false)) {
                 return false;
